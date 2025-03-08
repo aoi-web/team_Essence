199c65f1c1542094096385f6261225552ed668bc
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-03-09 02:53:59 +0530

    making the code updated

--- backend/flights/views.py
+++ backend/flights/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Flight
+from .serializers import FlightSerializer
 
-# Create your views here.
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
